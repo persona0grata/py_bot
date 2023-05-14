b9746cd90c79c6207ab2c6a659e29da95a729df2
Batyrkhan Tursumbekov <tumarbatr@gmail.com>
2023-05-14 17:06:34 +0600

    Co-authored-by: AZ4T <AZ4T@users.noreply.github.com>

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ bot = Bot(token=TG_TOKEN)
 dp = Dispatcher(bot=bot)
 
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['start', 'stop'])
 async def start_handler(message: types.Message):
     user_id = message.from_user.id
     user_name = message.from_user.first_name
@@ -21,9 +21,9 @@ async def start_handler(message: types.Message):
     logging.info(f'{user_id=} {user_full_name=} {time.asctime()}')
     await message.reply(f'Привет, {user_full_name}!')
     
-    for i in range(15):
+    while True:
         if(random.randint(1, 5) > 3):
-            time.sleep(2 * random.randint(1, 10))
+            time.sleep(2 * random.randint(1, 100))
             await bot.send_message(user_id, MSG.format(user_name))
         else:
             if random.randint(1, 7) > 3:
@@ -33,9 +33,20 @@ async def start_handler(message: types.Message):
                     await bot.send_message(user_id, "Прораб злой")
             else:
                 await bot.send_message(user_id, "Sigaret chekishni va gorilla ichishni xohlaysiz")
-            
-    
+        def stop_handler(message: types.Message):
+            break
     await bot.send_message(user_id, MSG2.format(user_name))
+    
+async def sigi(message: types.Message):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    user_full_name = message.from_user.full_name
+    logging.info(f'{user_id=} {user_full_name=} {time.asctime()}')
+    
+    while True:
+        time.sleep(30)
+        await bot.send_message(user_id, MSG.format(user_name))
+
         
         
 if __name__ == '__main__':
