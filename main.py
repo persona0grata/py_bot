0c48cbbd7dc5046661542cc9c3bad50145f59ba9
Batyrkhan Tursumbekov <tumarbatr@gmail.com>
2023-05-14 15:13:39 +0600

    added random

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import time
 import logging
+import random
 
 from aiogram import Bot, Dispatcher, executor, types
 
 
 TG_TOKEN = '5841858075:AAGG9s8xDHCOyTh4zYTmTHRQlsSaa3W5qcs'
 MSG = 'SART:{}! ПОРА РАБОТАТЬ!!!'
+MSG2 = 'SART:{}! ПЕРЕЗАПУСТИ БОТА!!!'
 
 bot = Bot(token=TG_TOKEN)
 dp = Dispatcher(bot=bot)
@@ -19,9 +21,11 @@ async def start_handler(message: types.Message):
     logging.info(f'{user_id=} {user_full_name=} {time.asctime()}')
     await message.reply(f'Привет, {user_full_name}!')
     
-    for i in range(10):
-        time.sleep(2)
+    for i in range(100):
+        time.sleep(2 * random.randint(1, 10))
         await bot.send_message(user_id, MSG.format(user_name))
+    
+    await bot.send_message(user_id, MSG2.format(user_name))
         
         
 if __name__ == '__main__':
